0750867389dad9249f3adc3c9f7e4a61f152068c
Felipe Barboza <felipebarboza5@gmail.com>
2022-07-02 15:16:42 -0300

    Configurando eslint no back e arrumando a configuração do package.json do front

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
 
-app.get('/', (req, res) => res.send('Hello Worldd!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => res.send('Hello Worldd!'));
+app.listen(port, () => (`Example app listening on port ${port}!`));
