c1ff7a48aed35b120f281baa0f8485f6dc9c7d43
Felipe Barboza <felipebarboza5@gmail.com>
2022-07-03 11:29:48 -0300

    Adicionando cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const connection = require('./src/models/connection');
 
 const app = express();
+app.use(cors());
 
 app.get('/', async (req, res) => {
-  const result = await connection.execute('SELECT * from task');
+  const [result] = await connection.execute('SELECT * from task');
   res.status(200).json(result);
 });
 
