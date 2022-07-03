843eef8c410bf97eb5c576c0a1e9e67812067721
Felipe Barboza <felipebarboza5@gmail.com>
2022-07-03 12:20:57 -0300

    Alterando umas coisinhas no app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+const axios = require('axios').default;
+
+const instance = axios.create({ baseURL: 'http://localhost:3001' });
 
 function App() {
-  const [task, setTask] = useState();
+  const [inputTask, setInputTask] = useState('');
+  const [task, setTask] = useState([]);
+
+  // Pega todas as tarefas
+  const getTask = async () => {
+    const result = await instance.get('/')
+      .then(({ data }) => setTask(data));
+    return result;
+  };
+
+  const updateInputTask = ({ target: { value } }) => {
+    setInputTask(value);
+  };
+
+  // Cria uma tarefa
+  const createTask = async () => {
+    await instance({
+      method: 'post',
+      data: { name: inputTask },
+    });
+    setInputTask('');
+  };
+
+  useEffect(() => {
+    getTask();
+  }, [task]);
 
   return (
-    <form type="submit">
-      <input type="text" placeholder="Add your new task" />
-      <button type="button">Create</button>
-    </form>
+    <div>
+      <form type="submit">
+        <input value={inputTask} onChange={updateInputTask} type="text" placeholder="Add your new task" />
+        <button onClick={createTask} type="button">Create</button>
+      </form>
+      {task.map((e) => (
+        <h1 key={e.id}>{e.name}</h1>
+      ))}
+    </div>
   );
 }
 
